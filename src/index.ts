557b6efe776e91fec09d253403b8e3d53f8f775d
PranayPant <ppantg1@gmail.com>
2023-02-23 20:07:17 -0600

    add position change

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as Redis from "redis";
 import { WebsocketRequestResponse } from "types/socket";
 import { geoAdd, geoSearch } from "interface/user";
 import { connect } from "interface/init";
-import { RedisGeoMember } from "types/redis";
+import { RedisGeoMember, RedisGeoSearchResult } from "types/redis";
 
 dotenv.config();
 
@@ -44,41 +44,62 @@ async function main() {
         const type = json.type;
         const member = json.data.member as string;
         switch (type) {
-          case "init":
+          case "ready-to-search": {
+            if (!SUBSCRIBERS.get(member)) {
+              SUBSCRIBERS.set(member, global.redis.duplicate());
+            }
+            const success = await geoAdd(json.data as RedisGeoMember);
+            const nearestUsers = await geoSearch(member, 2000, "km");
+            nearestUsers?.forEach((u) =>
+              SUBSCRIBERS.set(u.member, global.redis.duplicate())
+            );
+            socket.send(
+              success
+                ? `User ${member} is ready to search!`
+                : `Error: Failed to initialize search for user ${member}`
+            );
+            break;
+          }
+          case "searching-positions":
             {
-              if (!SUBSCRIBERS.get(member)) {
-                SUBSCRIBERS.set(member, global.redis.duplicate());
-              }
-              let addSuccess = await geoAdd(json.data as RedisGeoMember);
-              if (!addSuccess) {
-                socket.send("Error: Failed to initialize position");
+              if (!SUBSCRIBERS.size) {
+                socket.send(`Error: No active users found.`);
                 break;
               }
-              socket.send("Successfully initialized position!");
               const ret = await geoSearch(member, 2000, "km");
-              if (ret) {
-                socket.send(`Found ${ret.length - 1} users near you!`);
-                const userChannel = SUBSCRIBERS.get(member);
-                const subscribePromises = ret.map((r) => {
-                  return userChannel?.subscribe(r.member, (message) => {
-                    console.log(
-                      "User",
-                      member,
-                      `received a message from ${r.member}:`,
-                      `'${message}'`
-                    );
-                  });
-                });
-                await userChannel?.connect();
-                await Promise.all(subscribePromises);
+              if (!ret) {
+                socket.send(
+                  `Error: Failed to find any users near user ${member}`
+                );
+                break;
               }
+              socket.send(
+                `User ${member}, we found ${ret.length - 1} users near you!`
+              );
+              const userChannel = SUBSCRIBERS.get(member);
+              if (!userChannel) {
+                socket.send(`Error: User ${member} is not logged in.`);
+                break;
+              }
+              const subscribePromises = ret.map((r) => {
+                return userChannel.subscribe(r.member, (message) => {
+                  socket.send(
+                    `User ${member}: Hi user ${r.member}! You sent '${message}'`
+                  );
+                });
+              });
+              await userChannel.connect();
+              await Promise.all(subscribePromises);
             }
             break;
-          case "position":
+          case "changing-position":
             {
+              if (!SUBSCRIBERS.size) {
+                socket.send(`No users online to receive this message!`);
+              }
               global.redis.publish(
                 json.data.member,
-                `${json.data.member} updated his position.`
+                `User ${json.data.member} updated his position.`
               );
             }
             break;
